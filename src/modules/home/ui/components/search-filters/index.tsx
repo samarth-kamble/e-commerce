c0fc3770524fe2ffe7f4b59e3154895fb85df296
Samarth Kamble <kamblesamarth04@gmail.com>
2025-09-07 00:20:12 +0530

    10: Category Pages - Implement the category pages and breadcrumb navigation

--- src/modules/home/ui/components/search-filters/index.tsx
+++ src/modules/home/ui/components/search-filters/index.tsx
@@ -1,12 +1,55 @@
 "use client";
+
+import { useTRPC } from "@/trpc/client";
 import { Categories } from "./categories";
 import { SearchInput } from "./search-input";
-import { useTRPC } from "@/trpc/client";
 import { useSuspenseQuery } from "@tanstack/react-query";
+import { useParams } from "next/navigation";
+import { BreadcrumbNavigation } from "./breadcrumbs-navigation";
+import { DEFAULT_BG_COLOR } from "../../constants";
 
 export const SearchFilters = () => {
   const trpc = useTRPC();
   const { data } = useSuspenseQuery(trpc.categories.getMany.queryOptions());
+
+  const params = useParams();
+  const categoryParam = params.category as string | undefined;
+  const activeCategory = categoryParam || "all";
+
+  const activeCategoryData = data.find(
+    (category) => category.slug === activeCategory
+  );
+
+  const activeCategoryColor = activeCategoryData?.color || DEFAULT_BG_COLOR;
+  const activeCategoryName = activeCategoryData?.name || null;
+
+  const activeSubcategory = params.subcategory as string | undefined;
+  const activeSubcategoryName =
+    activeCategoryData?.subcategories?.find(
+      (subcategory) => subcategory.slug === activeSubcategory
+    )?.name || null;
+
+  return (
+    <div
+      className="px-4 lg:px-12 py-8 border-b flex flex-col gap-4 w-full"
+      style={{
+        backgroundColor: activeCategoryColor,
+      }}
+    >
+      <SearchInput data={data} />
+      <div className="hidden lg:block">
+        <Categories data={data} />
+      </div>
+      <BreadcrumbNavigation
+        activeCategory={activeCategory}
+        activeCategoryName={activeCategoryName}
+        activeSubcategoryName={activeSubcategoryName}
+      />
+    </div>
+  );
+};
+
+export const SearchFiltersSkeleton = () => {
   return (
     <div
       className="px-4 lg:px-12 py-8 border-b flex flex-col gap-4 w-full"
@@ -14,9 +57,9 @@ export const SearchFilters = () => {
         backgroundColor: "#F5F5F5",
       }}
     >
-      <SearchInput data={data} />
+      <SearchInput disabled data={[]} />
       <div className="hidden lg:block">
-        <Categories data={data} />
+        <div className="h-11" />
       </div>
     </div>
   );
--- src/modules/home/ui/components/search-filters/search-input.tsx
+++ src/modules/home/ui/components/search-filters/search-input.tsx
@@ -1,20 +1,21 @@
 "use client";
 
-import { useState } from "react";
-import { BookmarkCheckIcon, ListFilterIcon, SearchIcon } from "lucide-react";
-
 import { Input } from "@/components/ui/input";
-import { Button } from "@/components/ui/button";
+import { BookmarkCheckIcon, ListFilterIcon, SearchIcon } from "lucide-react";
+import { CustomCategory } from "../types";
 import { CategoriesSidebar } from "./categories-sidebar";
+import { useState } from "react";
+import { Button } from "@/components/ui/button";
 import { useTRPC } from "@/trpc/client";
 import { useQuery } from "@tanstack/react-query";
 import Link from "next/link";
 
 interface Props {
   disabled?: boolean;
+  data: CustomCategory[];
 }
 
-export const SearchInput = ({ disabled }: Props) => {
+export const SearchInput = ({ disabled, data }: Props) => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
   const trpc = useTRPC();
@@ -27,11 +28,10 @@ export const SearchInput = ({ disabled }: Props) => {
         <SearchIcon className="absolute left-3 top-1/2 -translate-y-1/2 size-4 text-neutral-500" />
         <Input
           className="pl-8"
-          placeholder="Search products"
+          placeholder="Search books"
           disabled={disabled}
         />
       </div>
-      {/* Add Category View All Button */}
       <Button
         variant="elevated"
         className="size-12 shrink-0 flex lg:hidden"
@@ -39,9 +39,10 @@ export const SearchInput = ({ disabled }: Props) => {
       >
         <ListFilterIcon />
       </Button>
+      {/* Add library button */}
       {session.data?.user && (
-        <Button asChild variant={"elevated"}>
-          <Link href={"/library"}>
+        <Button asChild variant="elevated">
+          <Link href="/library">
             <BookmarkCheckIcon />
             Library
           </Link>
