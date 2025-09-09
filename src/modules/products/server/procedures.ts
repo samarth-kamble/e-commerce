5e5fb2196400f2e8275f10a285e9ba4ca6e6f046
Samarth Kamble <kamblesamarth04@gmail.com>
2025-09-09 14:12:42 +0530

    12: Filtering Products - Fix bug 1

--- src/modules/products/server/procedures.ts
+++ src/modules/products/server/procedures.ts
@@ -13,11 +13,22 @@ export const productsRouter = createTRPCRouter({
       })
     )
     .query(async ({ ctx, input }) => {
-      const where: Where = {};
+      const where: Where = {
+        price: {},
+      };
 
-      if (input.minPrice) {
+      if (input.minPrice && input.maxPrice) {
         where.price = {
           greater_than_equal: input.minPrice,
+          less_than_equal: input.maxPrice,
+        };
+      } else if (input.minPrice) {
+        where.price = {
+          greater_than_equal: input.minPrice,
+        };
+      } else if (input.maxPrice) {
+        where.price = {
+          less_than_equal: input.maxPrice,
         };
       }
 
